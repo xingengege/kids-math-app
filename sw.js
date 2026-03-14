a4ccc9b2e3150d22af3ba3d7f95f3bf71b1c1f55
xingengege <123148490@qq.com>
2026-03-14 13:03:11 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker for 小小数学家
+const CACHE_NAME = 'math-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
